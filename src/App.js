d59dde1810ad9b0a7461e64cc2c5b1d4429dad08
Tushar <tushar789imran@gmail.com>
2022-12-07 21:45:35 +0600

    this project is done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import {
+  getDatabase,
+  ref,
+  set,
+  push,
+  onValue,
+  remove,
+  update,
+} from "firebase/database";
 
 function App() {
+  const db = getDatabase();
+
+  let [text, setText] = useState("");
+  let [text2, setText2] = useState("");
+  let [todo, setTodo] = useState([]);
+
+  let handelText = (e) => {
+    setText(e.target.value);
+  };
+
+  let handelSubmit = () => {
+    set(push(ref(db, "amar kaj")), {
+      kaj: text,
+    }).then(() => {
+      console.log("kaj hoiche");
+    });
+  };
+
+  useEffect(() => {
+    const todoRef = ref(db, "amar kaj");
+    onValue(todoRef, (snapshot) => {
+      let arr = [];
+      snapshot.forEach((item) => {
+        arr.push({ ...item.val(), id: item.key });
+      });
+      setTodo(arr);
+      console.log(arr);
+    });
+  }, []);
+
+  let handelDelete = (id) => {
+    remove(ref(db, "amar kaj/" + id)).then(() => {
+      console.log("kaj hoiche");
+    });
+  };
+
+  let handelEdit = (id) => {
+    update(ref(db, "amar kaj/" + id), {
+      kaj: text2,
+    }).then(() => {
+      console.log("update hoiche");
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <input onChange={handelText} />
+      <button onClick={handelSubmit}>Add</button>
+      <input onChange={(e) => setText2(e.target.value)} />
+
+      <ul>
+        {todo.map((item, index) => (
+          <li key={index}>
+            {item.kaj} {item.id}
+            <button onClick={() => handelEdit}>Edit</button>
+            <button onClick={() => handelDelete(item.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
 
---
+++ src/Config/firebaseConfig.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAWug9odPZijA-vp32Sop9OZOFOgDdHABw",
+  authDomain: "todo-a2f4b.firebaseapp.com",
+  projectId: "todo-a2f4b",
+  storageBucket: "todo-a2f4b.appspot.com",
+  messagingSenderId: "701581935335",
+  appId: "1:701581935335:web:61189a298e56c5a2ee9842",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default firebaseConfig;
